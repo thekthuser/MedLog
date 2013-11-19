3628bc89af675ff40009a68ff2a7f53ca0a2b8bb
thekthuser <thekthuser@thekthuser.com>
2013-11-19 15:16:37 -0500

    various changes

--- src/com/thekthuser/medlog/Manage.java
+++ src/com/thekthuser/medlog/Manage.java
@@ -2,11 +2,152 @@ package com.thekthuser.medlog;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class Manage extends BaseActivity {
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import android.widget.ExpandableListView;
+import android.app.ExpandableListActivity;
+import android.content.Context;
+import android.widget.ExpandableListView.OnChildClickListener;
+import android.view.LayoutInflater;
+
+import android.widget.SimpleExpandableListAdapter;
+import java.util.HashMap;
+import java.util.List;
+
+public class Manage extends BaseListActivity {
 
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.manage);
+        try {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.manage);
+
+            SimpleExpandableListAdapter xList = 
+                new SimpleExpandableListAdapter(
+                    this,
+                    createGroupList(),
+                    R.layout.manage_medication,
+                    new String[] {"Scientific Name", "Brand Name"},
+                    new int[] {R.id.scientific_name, R.id.brand_name},
+                    createChildList(),
+                    R.layout.manage_prescription,
+                    new String[] {"Sub Item"},
+                    new int[] {R.id.prescription}
+                );
+            getExpandableListView().setGroupIndicator(null);
+            setListAdapter(xList);
+        } catch (Exception e) {
+            System.out.println("EEEEEEEEEEEEEEEEEEEEEEEEEEE " + e.getMessage());
+        }
     }
+
+    private List createGroupList() {
+        ArrayList result = new ArrayList();
+        for(int i = 0; i < 4; i++) {
+            HashMap m = new HashMap();
+                m.put("Scientific Name", "Scientific Name" + Integer.toString(i));
+                m.put("Brand Name", "Brand Name" + Integer.toString(i));
+                result.add(m);
+        }
+        return (List) result;
+    }
+
+    private List createChildList() {
+        ArrayList result = new ArrayList();
+        for(int i = 0; i < 4; i++) {
+            ArrayList secList = new ArrayList();
+            for(int n = 0; n < 3; n++) {
+                HashMap child = new HashMap();
+                child.put("Sub Item", "Sub" + Integer.toString(n));
+            secList.add( child );
+            }
+        result.add(secList);
+        }
+        return result;
+    }
+
+    /*ArrayList<String> groupItem = new ArrayList<String>();
+    ArrayList<String> childItem = new ArrayList<String>();
+    groupItem.add("ONE");
+    groupItem.add("TWO");
+    groupItem.add("THREE");
+    
+    ArrayList<String> child = new ArrayList<String>();
+    child.add("1");
+    child.add("2");
+    child.add("3");
+    childItem.add(child);*/
+
+
+    /*ArrayList<String> groupItem = new ArrayList<String>();
+    ArrayList<Object> childItem = new ArrayList<Object>();
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);*/
+        //setContentView(R.layout.manage);
+        /*String[] values = new String[] { "one", "two", "three" };
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+            R.layout.manage_rowlayout, R.id.test, values);
+        setListAdapter(adapter);*/
+        /*ExpandableListView xList = getExpandableListView();
+        xList.setDividerHeight(2);
+        xList.setGroupIndicator(null);
+        xList.setClickable(true);
+        setGroupData();
+        setChildGroupData();
+
+        ManageExpandableListAdapter  mAdapter = new ManageExpandableListAdapter(groupItem, childItem);
+        mAdapter.setInflater((LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE), this);
+        getExpandableListView().setAdapter(mAdapter);
+        xList.setOnChildClickListener(this);
+    }
+
+    public void setGroupData() {
+        groupItem.add("ONE");
+        groupItem.add("TWO");
+        groupItem.add("THREE");
+    }
+    public void setChildGroupData() {
+        ArrayList<String> child = new ArrayList<String>();
+        child.add("1");
+        child.add("2");
+        child.add("3");
+        childItem.add(child);
+    }
+    @Override
+    public boolean onChildClick(ExpandableListView parent, View v, int groupPosition, int childPosition, long id) {
+        Toast.makeText(getBaseContext(), "Clicked", Toast.LENGTH_LONG).show();
+        return true;
+    }
+
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        String item = (String) getListAdapter().getItem(position);
+        Toast.makeText(this, item + " selected", Toast.LENGTH_LONG).show();
+    }*/
+
+    public void toggle_new_med(View view) {
+        View new_med = findViewById(R.id.new_med);
+        Button new_med_button = (Button) findViewById(R.id.toggle_new_med);
+        if (new_med.getVisibility() == View.VISIBLE) {
+            new_med.setVisibility(View.GONE);
+            new_med_button.setVisibility(View.VISIBLE);
+        } else {
+            new_med.setVisibility(View.VISIBLE);
+            new_med_button.setVisibility(View.GONE);
+        }
+    }
+
+    public void update_med(View view) {
+        EditText brand = (EditText) view.findViewById(R.id.brand_name);
+        EditText scientific = (EditText) view.findViewById(R.id.scientific_name);
+
+        toggle_new_med(view);
+    }
+
 }
---
+++ src/com/thekthuser/medlog/ManageAdapter.java
@@ -0,0 +1,81 @@
+package com.thekthuser.medlog;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.SQLException;
+import android.content.ContentValues;
+import android.database.Cursor;
+
+public class ManageAdapter {
+
+    private Context context;
+    private SQLiteDatabase db;
+    private SQLiteDatabase dbr;
+    private DatabaseHelper dbHelper;
+
+    public ManageAdapter(Context context) {
+        this.context = context;
+    }
+
+    public ManageAdapter open() throws SQLException {
+        dbHelper = new DatabaseHelper(context);
+        db = dbHelper.getWritableDatabase();
+        dbr = dbHelper.getReadableDatabase();
+        return this;
+    }
+
+    public void close() {
+        dbHelper.close();
+    }
+
+    public void addMedication(Medication medication) {
+        ContentValues cValues = new ContentValues();
+        cValues.put(DatabaseHelper.COLUMN_SCIENTIFIC_NAME, medication.getScientificName());
+        cValues.put(DatabaseHelper.COLUMN_BRAND_NAME, medication.getBrandName());
+        if (medication.getId() != 1) {
+            String sId = Integer.toString(medication.getId());
+            String where = DatabaseHelper.COLUMN_ID + " = ?";
+            String[] args = {
+                sId
+            };
+            db.update(DatabaseHelper.TABLE_MEDICATION, cValues, where, args);
+        } else {
+            db.insert(DatabaseHelper.TABLE_MEDICATION, null, cValues);
+        }
+    }
+
+
+    public Medication getMedication(int id) {
+        String[] projection = {
+            DatabaseHelper.COLUMN_ID,
+            DatabaseHelper.COLUMN_SCIENTIFIC_NAME,
+            DatabaseHelper.COLUMN_BRAND_NAME
+        };
+        String selection = DatabaseHelper.COLUMN_ID + " = ?";
+        String sId = Integer.toString(id);
+        String[] selectionArgs = {
+            sId
+        };
+        String order = DatabaseHelper.COLUMN_ID + " DESC LIMIT 1";
+
+        Cursor cursor = dbr.query(
+            DatabaseHelper.TABLE_MEDICATION,
+            projection,
+            selection,
+            selectionArgs,
+            null,
+            null,
+            order
+        );
+
+        Medication medication;
+        if (cursor.moveToFirst()) {
+            medication = new Medication(cursor.getInt(0), cursor.getString(1), cursor.getString(2));
+        } else {
+            medication = new Medication(-1, "Scientific Name", "Brand Name");
+        }
+        return medication;
+    }
+
+
+}
