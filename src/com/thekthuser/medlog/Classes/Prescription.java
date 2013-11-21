ab04c9d5ea98bfee4879fc17eec9f3eb65bb6c09
thekthuser <thekthuser@thekthuser.com>
2013-11-21 13:50:20 -0500

    added medication_id to Prescription.java

--- src/com/thekthuser/medlog/Classes/Prescription.java
+++ src/com/thekthuser/medlog/Classes/Prescription.java
@@ -6,14 +6,16 @@ public class Prescription {
     private String pill_dosage;
     private String dosage_taken;
 
-    public Prescription(int id, String pill_dosage, String dosage_taken) {
+    public Prescription(int id, int medication_id, String pill_dosage, String dosage_taken) {
         this.id = id;
+        this.medication_id = medication_id;
         this.pill_dosage = pill_dosage;
         this.dosage_taken = dosage_taken;
     }
 
-    public Prescription(String pill_dosage, String dosage_taken) {
+    public Prescription(int medication_id, String pill_dosage, String dosage_taken) {
         this.id = -1;
+        this.medication_id = medication_id;
         this.pill_dosage = pill_dosage;
         this.dosage_taken = dosage_taken;
     }
@@ -24,6 +26,14 @@ public class Prescription {
 
     public void setId(int id) {
         this.id = id;
+    }
+
+    public int getMedicationId() {
+        return medication_id;
+    }
+
+    public void setMedicationId(int medication_id) {
+        this.medication_id = medication_id;
     }
 
     public String getPillDosage() {
